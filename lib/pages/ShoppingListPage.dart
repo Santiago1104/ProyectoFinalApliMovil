d54ae09e100cb5ff8e7a270f9d00fbdb7139dd0f
AndreaC2000 <andreacuatindioy226@gmail.com>
2024-12-10 11:07:08 -0500

    se añadio el boton copiar lista

--- lib/pages/ShoppingListPage.dart
+++ lib/pages/ShoppingListPage.dart
@@ -3,10 +3,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import '../models/ShoppingList.dart';
 import '../models/Product.dart';
 import '../models/Site.dart';
-import 'EditProductPage.dart';
-import 'CreateProductPage.dart';
-import 'CreateShoppingListModal.dart';
-import '../services/firebase_service.dart';
+import 'EditProductPage.dart'; // Importa la nueva página
+import 'CreateProductPage.dart'; // Asegúrate de importar esta página
+import 'CreateShoppingListModal.dart'; // Importar la nueva ventana modal
 
 class ShoppingListPage extends StatefulWidget {
   @override
@@ -20,8 +19,9 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
         .orderBy('FechaRegistro', descending: true)
         .snapshots()
         .map((snapshot) => snapshot.docs
-        .map((doc) => ShoppingList.fromFirestore(doc.data() as Map<String, dynamic>, doc.id))
-        .toList());
+            .map((doc) => ShoppingList.fromFirestore(
+                doc.data() as Map<String, dynamic>, doc.id))
+            .toList());
   }
 
   Stream<List<Product>> readProducts(String listId) {
@@ -31,8 +31,9 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
         .collection('elementoslista')
         .snapshots()
         .map((snapshot) => snapshot.docs
-        .map((doc) => Product.fromFirestore(doc.data() as Map<String, dynamic>, doc.id))
-        .toList());
+            .map((doc) => Product.fromFirestore(
+                doc.data() as Map<String, dynamic>, doc.id))
+            .toList());
   }
 
   Future<Site?> getSiteDetails(String siteId) async {
@@ -51,7 +52,6 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
     return null;
   }
 
-<<<<<<< HEAD
   void deleteProduct(String listId, String productId) async {
     if (listId.isNotEmpty && productId.isNotEmpty) {
       await FirebaseFirestore.instance
@@ -77,9 +77,11 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
           .collection('elementoslista')
           .get();
 
-      // Crear una nueva lista clonada con el nombre proporcionado
-      final newListDoc = await FirebaseFirestore.instance.collection('lista_compras').add({
-        'nombre': listName, // Guardar correctamente el nombre con la clave consistente
+      // Crear una nueva lista clonada
+      final newListDoc =
+          await FirebaseFirestore.instance.collection('lista_compras').add({
+        'nombre': listName,
+        // Guardar correctamente el nombre con la clave consistente
         'FechaRegistro': DateTime.now(),
       });
 
@@ -91,7 +93,6 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
             .collection('elementoslista')
             .add(productDoc.data());
       }
-
       // Mostrar un mensaje de éxito con el nuevo nombre
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Lista "$listName" clonada con éxito.')),
@@ -104,9 +105,10 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
     }
   }
 
-  Future<void> showCloneDialog(String originalListId, String originalListName) async {
-    final TextEditingController nameController = TextEditingController(text: 'Copia de $originalListName');
-
+  Future<void> showCloneDialog(
+      String originalListId, String originalListName) async {
+    final TextEditingController nameController =
+        TextEditingController(text: 'Copia de $originalListName');
     await showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -143,11 +145,6 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
     );
   }
 
-
-
-
-=======
->>>>>>> 6e89dbd9ac5e5307a27a7b206b38dd1d07ad3ae2
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -173,151 +170,136 @@ class _ShoppingListPageState extends State<ShoppingListPage> {
           } else if (listSnapshot.hasData) {
             final lists = listSnapshot.data!;
             if (lists.isEmpty) {
-              return Center(child: Text('No hay listas de compras disponibles.'));
+              return Center(
+                  child: Text('No hay listas de compras disponibles.'));
             }
             return ListView.builder(
               itemCount: lists.length,
               itemBuilder: (context, index) {
                 final shoppingList = lists[index];
-                return Dismissible(
-                  key: ValueKey(shoppingList.id),
-                  direction: DismissDirection.startToEnd,
-                  onDismissed: (direction) async {
-                    await deleteShoppingList(shoppingList.id);
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      SnackBar(content: Text('Lista eliminada')),
-                    );
-                  },
-                  background: Container(
-                    color: Colors.red,
-                    alignment: Alignment.centerLeft,
-                    padding: EdgeInsets.only(left: 20.0),
-                    child: Icon(Icons.delete, color: Colors.white),
-                  ),
-                  child: ExpansionTile(
-                    title: Text(
-                      shoppingList.name,
-                      style: TextStyle(fontSize: 18.0),
-                    ),
-                    subtitle: Text('Fecha: ${shoppingList.date}'),
-                    children: [
-                      StreamBuilder<List<Product>>(
-                        stream: readProducts(shoppingList.id),
-                        builder: (context, productSnapshot) {
-                          if (productSnapshot.hasError) {
-                            return Center(child: Text('Error: ${productSnapshot.error}'));
-                          } else if (productSnapshot.hasData) {
-                            final products = productSnapshot.data!;
-                            if (products.isEmpty) {
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Text('No hay productos en esta lista.'),
-                              );
-                            }
-                            return ListView.builder(
-                              shrinkWrap: true,
-                              physics: NeverScrollableScrollPhysics(),
-                              itemCount: products.length,
-                              itemBuilder: (context, index) {
-                                final product = products[index];
-                                return FutureBuilder<Site?>(
-                                  future: getSiteDetails(product.siteId),
-                                  builder: (context, siteSnapshot) {
-                                    if (siteSnapshot.hasError) {
-                                      return ListTile(
-                                        title: Text(product.name),
-                                        subtitle: Text('Error al cargar el sitio'),
-                                      );
-                                    } else if (siteSnapshot.hasData) {
-                                      final site = siteSnapshot.data!;
-                                      return Dismissible(
-                                        key: ValueKey(product.id),
-                                        confirmDismiss: (direction) async {
-                                          if (!product.isChecked) {
-                                            return true;
-                                          }
-                                          return false;
-                                        },
-                                        onDismissed: (direction) {
-                                          if (!product.isChecked) {
-                                            deleteProduct(shoppingList.id, product.id);
-                                          }
-                                        },
-                                        background: Container(
-                                          color: Colors.red,
-                                          child: Align(
-                                            alignment: Alignment.centerLeft,
-                                            child: Padding(
-                                              padding: const EdgeInsets.only(left: 16.0),
-                                              child: Icon(Icons.delete, color: Colors.white),
-                                            ),
+                return ExpansionTile(
+                  title: Text(shoppingList.name),
+                  subtitle: Text('Fecha: ${shoppingList.date}'),
+                  children: [
+                    StreamBuilder<List<Product>>(
+                      stream: readProducts(shoppingList.id),
+                      builder: (context, productSnapshot) {
+                        if (productSnapshot.hasError) {
+                          return Center(
+                              child: Text('Error: ${productSnapshot.error}'));
+                        } else if (productSnapshot.hasData) {
+                          final products = productSnapshot.data!;
+                          if (products.isEmpty) {
+                            return Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text('No hay productos en esta lista.'),
+                            );
+                          }
+                          return ListView.builder(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: products.length,
+                            itemBuilder: (context, index) {
+                              final product = products[index];
+                              return FutureBuilder<Site?>(
+                                future: getSiteDetails(product.siteId),
+                                builder: (context, siteSnapshot) {
+                                  if (siteSnapshot.hasError) {
+                                    return ListTile(
+                                      title: Text(product.name),
+                                      subtitle:
+                                          Text('Error al cargar el sitio'),
+                                    );
+                                  } else if (siteSnapshot.hasData) {
+                                    final site = siteSnapshot.data!;
+                                    return Dismissible(
+                                      key: ValueKey(product.id),
+                                      confirmDismiss: (direction) async {
+                                        if (!product.isChecked) {
+                                          return true;
+                                        }
+                                        return false;
+                                      },
+                                      onDismissed: (direction) {
+                                        if (!product.isChecked) {
+                                          deleteProduct(
+                                              shoppingList.id, product.id);
+                                        }
+                                      },
+                                      background: Container(
+                                        color: Colors.red,
+                                        child: Align(
+                                          alignment: Alignment.centerLeft,
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                                left: 16.0),
+                                            child: Icon(Icons.delete,
+                                                color: Colors.white),
                                           ),
                                         ),
-                                        child: ListTile(
-                                          title: Text(product.name),
-                                          subtitle: Text('Lugar: ${site.name}'),
-                                          trailing: Checkbox(
-                                            value: product.isChecked,
-                                            onChanged: (value) async {
-                                              await product.toggleChecked();
-                                              setState(() {});
-                                            },
-                                          ),
-                                          onTap: () {
-                                            Navigator.push(
-                                              context,
-                                              MaterialPageRoute(
-                                                builder: (context) => EditProductPage(
-                                                  listId: shoppingList.id,
-                                                  productId: product.id,
-                                                ),
-                                              ),
-                                            );
+                                      ),
+                                      child: ListTile(
+                                        title: Text(product.name),
+                                        subtitle: Text('Lugar: ${site.name}'),
+                                        trailing: Checkbox(
+                                          value: product.isChecked,
+                                          onChanged: (value) async {
+                                            await product.toggleChecked();
+                                            setState(() {});
                                           },
                                         ),
-                                      );
-                                    } else {
-                                      return Center(child: CircularProgressIndicator());
-                                    }
-                                  },
-                                );
-                              },
-                            );
-                          } else {
-                            return Center(child: CircularProgressIndicator());
-                          }
-                        },
-                      ),
-<<<<<<< HEAD
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (context) =>
+                                                  EditProductPage(
+                                                listId: shoppingList.id,
+                                                productId: product.id,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    );
+                                  } else {
+                                    return Center(
+                                        child: CircularProgressIndicator());
+                                  }
+                                },
+                              );
+                            },
+                          );
+                        } else {
+                          return Center(child: CircularProgressIndicator());
+                        }
+                      },
                     ),
                     Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: ElevatedButton(
-                        onPressed: () => showCloneDialog(shoppingList.id, shoppingList.name),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => CreateProductPage(
+                                listId: shoppingList.id,
+                              ),
+                            ),
+                          );
+                        },
+                        child: Text('Añadir Producto'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ElevatedButton(
+                        onPressed: () =>
+                            showCloneDialog(shoppingList.id, shoppingList.name),
                         child: Text('Clonar Lista'),
                       ),
                     ),
                   ],
-=======
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => CreateProductPage(
-                                  listId: shoppingList.id,
-                                ),
-                              ),
-                            );
-                          },
-                          child: Text('Añadir Producto'),
-                        ),
-                      ),
-                    ],
-                  ),
->>>>>>> 6e89dbd9ac5e5307a27a7b206b38dd1d07ad3ae2
                 );
               },
             );

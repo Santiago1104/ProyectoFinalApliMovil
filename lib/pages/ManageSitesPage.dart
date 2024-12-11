b7ef13c20af9af0c34753503ef4ba3465c79ece9
Elizabeth <gloriaeq@unicauca.edu.co>
2024-12-11 00:00:08 -0500

    Menú desplegable con página para Administrar Sitios y Actualización Tema

---
+++ lib/pages/ManageSitesPage.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+import '../models/Site.dart';
+import '../services/firebase_service.dart';
+import 'AddSite.dart';
+
+class ManageSitesPage extends StatefulWidget {
+  @override
+  _ManageSitesPageState createState() => _ManageSitesPageState();
+}
+
+class _ManageSitesPageState extends State<ManageSitesPage> {
+  final TextEditingController _siteNameController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Sitios"),
+      ),
+      body: StreamBuilder<List<Site>>(
+        stream: readSites(),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Text("Error: ${snapshot.error}");
+          }
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final sites = snapshot.data!;
+          return ListView.builder(
+            itemCount: sites.length,
+            itemBuilder: (context, index) {
+              Site site = sites[index];
+              return ListTile(
+                title: Text(site.name),
+                trailing: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: <Widget>[
+                    IconButton(
+                      icon: Icon(Icons.edit),
+                      onPressed: () => _showEditSiteDialog(site),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.delete),
+                      onPressed: () => _deleteSite(site.id),
+                    ),
+                  ],
+                ),
+              );
+            },
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color(0xFF9c8def),
+        child: Icon(Icons.add),
+          onPressed: () {
+            showDialog(
+              context: context,
+              builder: (context) =>
+              AddSite(
+                onAdd: (siteName) {
+                  addSite(
+                      siteName);
+                },
+              ),
+            );
+          },
+      ),
+    );
+  }
+
+  void _showEditSiteDialog(Site site) {
+    _siteNameController.text = site.name;  // Precarga el nombre actual del sitio en el controlador
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Editar Sitio'),
+          content: TextField(
+            controller: _siteNameController,
+            decoration: InputDecoration(hintText: "Nombre del sitio"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancelar'),
+              onPressed: () {
+                Navigator.of(context).pop();
+                _siteNameController.clear();
+              },
+            ),
+            TextButton(
+              child: Text('Actualizar'),
+              onPressed: () {
+                if (_siteNameController.text.isNotEmpty) {
+                  Site updatedSite = Site(
+                      id: site.id,
+                      name: _siteNameController.text,
+                      date: site.date  // Mantiene la fecha original de registro
+                  );
+                  updateSite(updatedSite);
+                  Navigator.of(context).pop();
+                  _siteNameController.clear();
+                }
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _deleteSite(String siteId) {
+    deleteSite(siteId);
+  }
+}
